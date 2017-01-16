ac172b6be265f08cd6fcbcd921369548c670ea59
iOS Student <iosstudent@techmasters-imac.local>
2017-01-16 09:40:44 +0700

    Initial Commit

---
+++ EzUIWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EzUIWebView
+//
+//  Created by iOS Student on 1/16/17.
+//  Copyright © 2017 tek4fun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
